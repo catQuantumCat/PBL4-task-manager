9678fdfcb93d4cae9f5174839cfe1be51545129e
naeatammy <tamnguyendacnguyen@gmail.com>
2024-10-07 10:41:15 +0700

    feat: login/signup using jwt

--- backend/Data/ApplicationDBContext.cs
+++ backend/Data/ApplicationDBContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using backend.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace backend.Data
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext<AppUser>
     {
         public ApplicationDBContext (DbContextOptions dbContextOptions)
         :base(dbContextOptions)
@@ -18,5 +20,37 @@ namespace backend.Data
         public DbSet<Mission> Missions { get; set;}
         public DbSet<User> Users { get; set;}
         
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // builder.Entity<Portfolio>(x => x.HasKey(p => new {p.AppUserId, p.StockId}));
+
+            // builder.Entity<Portfolio>()
+            //  .HasOne(u => u.AppUser)
+            //  .WithMany(u => u.Portfolios)
+            //  .HasForeignKey(p => p.AppUserId);
+
+            // builder.Entity<Portfolio>()
+            //  .HasOne(u => u.Stock)
+            //  .WithMany(u => u.Portfolios)
+            //  .HasForeignKey(p => p.StockId);
+
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
